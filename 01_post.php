235de6a597221405af2acdbf43b4c512e4b24e35
toshiharocky <toshiharu69@gmail.com>
2021-03-22 22:38:28 +0900

    value変更

--- 01_post.php
+++ 01_post.php
@@ -19,27 +19,27 @@
     
         <p>メールアドレス：<input type="text" name="mail"></p> <!-- メールアドレス -->
 
-        <p>参加可能日</p>
-        <input type="checkbox" name="date[]" value="1">4月2日 <!-- 参加可能日(チェックボックス) -->
-        <input type="checkbox" name="date[]" value="2">4月3日
-        <input type="checkbox" name="date[]" value="3">4月9日
-        <input type="checkbox" name="date[]" value="5">4月10日
-        <input type="checkbox" name="date[]" value="4">4月16日
-        <input type="checkbox" name="date[]" value="6">4月17日
+        <p>参加可能日</p> <!-- 参加可能日(チェックボックス) -->
+        <input type="checkbox" name="date[]" value="4月2日">4月2日
+        <input type="checkbox" name="date[]" value="4月3日">4月3日
+        <input type="checkbox" name="date[]" value="4月9日">4月9日
+        <input type="checkbox" name="date[]" value="4月10日">4月10日
+        <input type="checkbox" name="date[]" value="4月16日">4月16日
+        <input type="checkbox" name="date[]" value="4月17日">4月17日
     
-        <p>場所</p>
-        <input type="checkbox" name="place" value="1">新宿 <!-- 場所(チェックボックス) -->
-        <input type="checkbox" name="place" value="2">恵比寿
-        <input type="checkbox" name="place" value="3">渋谷
-        <input type="checkbox" name="place" value="4">新橋
-        <input type="checkbox" name="place" value="5">上野
-        <input type="checkbox" name="place" value="6">赤羽
+        <p>場所</p> <!-- 場所(チェックボックス) -->
+        <input type="checkbox" name="place[]" value="新宿">新宿
+        <input type="checkbox" name="place[]" value="恵比寿">恵比寿
+        <input type="checkbox" name="place[]" value="渋谷">渋谷
+        <input type="checkbox" name="place[]" value="新橋">新橋
+        <input type="checkbox" name="place[]" value="上野">上野
+        <input type="checkbox" name="place[]" value="赤羽">赤羽
         
-        <p>食事タイプ</p>
-        <input type="checkbox" name="food-type" value="1">和食 <!-- 食事タイプ(チェックボックス) -->
-        <input type="checkbox" name="food-type" value="2">中華
-        <input type="checkbox" name="food-type" value="3">イタリアン
-        <input type="checkbox" name="food-type" value="4">フレンチ
+        <p>食事タイプ</p> <!-- 食事タイプ(チェックボックス) -->
+        <input type="checkbox" name="food-type[]" value="和食">和食
+        <input type="checkbox" name="food-type[]" value="中華">中華
+        <input type="checkbox" name="food-type[]" value="イタリアン">イタリアン
+        <input type="checkbox" name="food-type[]" value="フレンチ">フレンチ
     
         <p>アレルギー<input type="text" name="allergy"></p> <!-- アレルギー -->
     
