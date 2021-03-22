3d735196cd345d095a939d897199bd64cff562d6
toshiharocky <toshiharu69@gmail.com>
2021-03-22 13:46:40 +0900

    CSS設定

--- 01_post.php
+++ 01_post.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="quetionnaire.css">
 </head>
 <body>
 <h1>〇〇に関するアンケート</h1>
-<h2>氏名</h2>
-<input type="text" action="02_post_confirm.php" name="name">
+
+<!-- 質問事項 -->
+<div class="question">
+    <h2>氏名</h2>
+    <input type="text" action="02_post_confirm.php" name="name">
+</div>
 
     
 </body>
