b106bc6ba10eb1ce317f5532eedefefa828ace79
toshiharocky <toshiharu69@gmail.com>
2021-03-22 19:21:41 +0900

    記入項目定義

--- 01_post.php
+++ 01_post.php
@@ -8,25 +8,39 @@
     <link rel="stylesheet" href="questionnaire.css">
 </head>
 <body>
+
+
 <h1>〇〇に関するアンケート</h1>
 
 <!-- 質問事項（POSTで送信） -->
 <div id="questions-wrapper">
-    <form action="02_post_confirm.php" method="post">
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- 氏名 -->
         <p>氏名：<input type="text" name="name"></p>
     </form>
-    <form action=""class="questions"></form>
-    <form action=""class="questions"></form>
-    <form action=""class="questions"></form>
-    <form action=""class="questions"></form>
-    <form action=""class="questions"></form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- メールアドレス -->
+
+    </form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- 参加可能日(チェックボックス) -->
+
+    </form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- 場所(チェックボックス) -->
+
+    </form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- 食事タイプ(チェックボックス) -->
+
+    </form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- アレルギー -->
+
+    </form>
+    <form action="02_post_confirm.php" method="post" class="questions"> <!-- その他 -->
+
+    </form>
 </div>
 
 <!-- 確認ボタン -->
     
+
+
 </body>
 </html>
 
-
-<?
-?>
