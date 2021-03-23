a2ee6f405617d5b6da79a99eaffea15c1734efde
toshiharocky <toshiharu69@gmail.com>
2021-03-23 17:33:05 +0900

    トップページへ戻るボタンを追加

--- 04_completed.php
+++ 04_completed.php
@@ -15,12 +15,13 @@ $time = date('m/d H:i:s');
 // ファイル(data.json)に書き込み
 $file = fopen("data/data.json", "a");
 session_start();
-fwrite($file, $time." ".$_SESSION["jsonForRecord"]."\n");
+fwrite($file, $_SESSION["jsonForRecord"]."\n");
 fclose($file);
 ?>
 
 
 <h1>送信完了しました</h1>
+<button onclick="location.href='01_post.php'" >入力画面に戻る
 
 </body>
 </html>
