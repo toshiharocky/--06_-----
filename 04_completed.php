6d973ac20aa22189611770091bf993cc2dd88670
toshiharocky <toshiharu69@gmail.com>
2021-03-23 17:24:50 +0900

    ページ遷移後にファイル(data.json)に書き込み

--- 04_completed.php
+++ 04_completed.php
@@ -7,5 +7,20 @@
 </head>
 <body>
     
+
+<?php
+// 時間データ取得
+$time = date('m/d H:i:s');
+
+// ファイル(data.json)に書き込み
+$file = fopen("data/data.json", "a");
+session_start();
+fwrite($file, $time." ".$_SESSION["jsonForRecord"]."\n");
+fclose($file);
+?>
+
+
+<h1>送信完了しました</h1>
+
 </body>
 </html>
